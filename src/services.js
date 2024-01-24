9bd89a94ba5c748e95f7e6c0f780ab911396508d
Celia <celiamillanalemany@gmail.com>
2024-01-24 23:20:36 +0100

    delete method

--- src/services.js
+++ src/services.js
@@ -15,8 +15,8 @@ async function updateBooks(id, editedBook){
 
 }
 //DELETE
-async function deleteBooks(id){
-
+async function deleteBook(id){
+    await fetch(`http://localhost:3000/books/${id}`, { method: 'DELETE'})
 }
 
 //imprimir 
@@ -24,8 +24,13 @@ let booksContainer = document.querySelector('section')
 async function printBooks(){
     console.log('hola')
     let books = await getBooks();
-    const bookList = books.map(book => {
-        return booksContainer.innerHTML += `<h1>${book.title}</h1>`
+    const bookList = await books.map(book => {
+        return booksContainer.innerHTML +=
+         `<h1>${book.title}</h1>
+         <p>${book.writer}</p>
+         <p>${book.book_description}</p>
+         <button onClick=deleteBook(${book.id})>Delete</button>
+         `
     });
 
     return bookList
